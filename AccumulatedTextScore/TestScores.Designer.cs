87e4e1f83153bf22ef0aac10539b917058b5b23a
Momo Johnson <mo12g13@gmail.com>
2016-12-04 14:05:12 -0600

    Done with  this project

---
+++ AccumulatedTextScore/TestScores.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace AccumulatedTextScore
+{
+    partial class TestScores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtScoreInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblscoreTotal = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblScoreCount = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSortedScore = new System.Windows.Forms.Button();
+            this.btnClearScores = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblMaxValue = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblMinValue = new System.Windows.Forms.Label();
+            this.btnUnscoredGrades = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(60, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score: ";
+            // 
+            // txtScoreInput
+            // 
+            this.txtScoreInput.Location = new System.Drawing.Point(138, 32);
+            this.txtScoreInput.Name = "txtScoreInput";
+            this.txtScoreInput.Size = new System.Drawing.Size(70, 20);
+            this.txtScoreInput.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Score Total:";
+            // 
+            // lblscoreTotal
+            // 
+            this.lblscoreTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblscoreTotal.Location = new System.Drawing.Point(139, 63);
+            this.lblscoreTotal.Name = "lblscoreTotal";
+            this.lblscoreTotal.Size = new System.Drawing.Size(69, 20);
+            this.lblscoreTotal.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Score Count:";
+            // 
+            // lblScoreCount
+            // 
+            this.lblScoreCount.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblScoreCount.Location = new System.Drawing.Point(138, 97);
+            this.lblScoreCount.Name = "lblScoreCount";
+            this.lblScoreCount.Size = new System.Drawing.Size(70, 20);
+            this.lblScoreCount.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(23, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 20);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "     Average:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAverage.Location = new System.Drawing.Point(138, 140);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(69, 20);
+            this.lblAverage.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(240, 26);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(83, 26);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSortedScore
+            // 
+            this.btnSortedScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSortedScore.Location = new System.Drawing.Point(26, 178);
+            this.btnSortedScore.Name = "btnSortedScore";
+            this.btnSortedScore.Size = new System.Drawing.Size(123, 28);
+            this.btnSortedScore.TabIndex = 4;
+            this.btnSortedScore.Text = "Sorted Grade";
+            this.btnSortedScore.UseVisualStyleBackColor = true;
+            this.btnSortedScore.Click += new System.EventHandler(this.sorrtedScores_Click);
+            // 
+            // btnClearScores
+            // 
+            this.btnClearScores.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClearScores.Location = new System.Drawing.Point(26, 214);
+            this.btnClearScores.Name = "btnClearScores";
+            this.btnClearScores.Size = new System.Drawing.Size(123, 28);
+            this.btnClearScores.TabIndex = 5;
+            this.btnClearScores.Text = "Clear Scores";
+            this.btnClearScores.UseVisualStyleBackColor = true;
+            this.btnClearScores.Click += new System.EventHandler(this.btnClearScores_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(237, 212);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(86, 30);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblMaxValue
+            // 
+            this.lblMaxValue.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMaxValue.Location = new System.Drawing.Point(241, 83);
+            this.lblMaxValue.Name = "lblMaxValue";
+            this.lblMaxValue.Size = new System.Drawing.Size(82, 20);
+            this.lblMaxValue.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(240, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Max Value";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(237, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Min Value";
+            // 
+            // lblMinValue
+            // 
+            this.lblMinValue.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMinValue.Location = new System.Drawing.Point(243, 139);
+            this.lblMinValue.Name = "lblMinValue";
+            this.lblMinValue.Size = new System.Drawing.Size(80, 21);
+            this.lblMinValue.TabIndex = 10;
+            // 
+            // btnUnscoredGrades
+            // 
+            this.btnUnscoredGrades.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUnscoredGrades.Location = new System.Drawing.Point(183, 178);
+            this.btnUnscoredGrades.Name = "btnUnscoredGrades";
+            this.btnUnscoredGrades.Size = new System.Drawing.Size(140, 29);
+            this.btnUnscoredGrades.TabIndex = 11;
+            this.btnUnscoredGrades.Text = "Unsorted Grades";
+            this.btnUnscoredGrades.UseVisualStyleBackColor = true;
+            this.btnUnscoredGrades.Click += new System.EventHandler(this.btnUnscoredGrades_Click);
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 2);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(205, 27);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Project 8-1 Momo Johnson";
+            // 
+            // TestScores
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(335, 254);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnUnscoredGrades);
+            this.Controls.Add(this.lblMinValue);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblMaxValue);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClearScores);
+            this.Controls.Add(this.btnSortedScore);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.lblScoreCount);
+            this.Controls.Add(this.lblscoreTotal);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtScoreInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "TestScores";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Score Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtScoreInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblscoreTotal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblScoreCount;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSortedScore;
+        private System.Windows.Forms.Button btnClearScores;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblMaxValue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblMinValue;
+        private System.Windows.Forms.Button btnUnscoredGrades;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ AccumulatedTextScore/TestScores.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*Author: Momo Johnson
+ * C Sharp Project 8
+ * This application calculates the total, finds the minimum, maximimum, sorted, unsorted and amount of 
+ * test scores enter by the user
+ * 
+ * */
+namespace AccumulatedTextScore
+{
+    public partial class TestScores : Form
+    {
+        
+        List<int> gradeList = new List<int>();
+        public TestScores()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //An action listener that add grades to the grade list when click
+        private void btnAdd_Click(object sender, EventArgs e)
+        {            
+           try
+            {
+                //A validate method that validate the inputs enter by the user
+                if (isValidData())
+                {
+                       //Parsing of the string to an integer
+                    int number = Int32.Parse(txtScoreInput.Text);
+                    //Adding of grades to the gradeList
+                    gradeList.Add(number);
+                    //Setting the total grades score using the gradeTotal method
+                    lblscoreTotal.Text = gradeTotal(gradeList).ToString();
+                    //Setting the average grade using the method 
+                    lblAverage.Text = average(gradeList).ToString();
+                    //Setting the score count using the score count method
+                    lblScoreCount.Text = gradeCount(gradeList).ToString();
+                    int max = gradeList.Max();
+                    int min = gradeList.Min();
+                    lblMaxValue.Text = "" + max;
+                    lblMinValue.Text = "" + min;               
+                }
+            }catch(Exception)
+            {
+                //If an exception occur Show a message to the user
+                MessageBox.Show("An Exception has occured");
+            }
+            //Clearing the score inputs and Setting the focus back in the text box
+            txtScoreInput.Clear();
+            txtScoreInput.Focus();              
+           
+        }
+           
+        // A method that checks if the user didn't enter any string
+        private bool isPresent(TextBox textBox, String name)
+        {
+            //If the textBox is empty, show a message telling the user that the textBox
+            if(textBox.Text == "")
+            {
+                MessageBox.Show(name + " is a required field");
+                textBox.Focus();
+                textBox.Clear();
+                return false;
+            }
+            return true;
+        }
+        //A method that checks if the string enter in a textBox is an integer and if is greater than zero and less or equal to 100
+        private bool isInteger(TextBox textBox, String name)
+        {
+            int number;
+            if(Int32.TryParse(textBox.Text, out number))
+            {
+                if(number > 0 && number <= 100)
+                {
+                    return true;
+                }
+            }
+            //If the string isn't a number, show a message and ask the user to enter a number between 1-100
+            MessageBox.Show(name + " must be an Integer from 1-100");
+            textBox.Focus();
+            textBox.Clear();
+            return false;
+        }
+        //A method that validates the input enter by the user
+        public bool isValidData()
+        {
+            if (!isPresent(txtScoreInput, "Score"))
+                return false;
+            if (!isInteger(txtScoreInput, "Score"))
+                return false;
+            return true;
+        }
+        //A Action event that display the scores enter by the user
+       
+        //A method that calculate the grade total from the list
+        private int gradeTotal(List<int> grades)
+        {
+            int number=0;
+            foreach (int grade in gradeList)
+                number += grade;
+           return number;           
+        }
+
+        //A method that counts the number grades enter by the user
+        private int gradeCount(List<int> countGrades)
+        {
+            return countGrades.Count();
+        }
+        //A method that calculates the average of the grades
+        private int average(List<int> grades)
+        {
+            int value =0;
+            foreach (int number in grades)
+                value += number;
+            return value / grades.Count();
+
+        }
+        
+        // An action event that that clears the scores enter by the user 
+        private void btnClearScores_Click(object sender, EventArgs e)
+        {
+            //Dialog result that clears the input if the user presses the yes button
+            DialogResult result = MessageBox.Show("Are you sure you want to clear the scores", "Confirmation?", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+            if(result == DialogResult.Yes)
+            {
+                gradeList.Clear();
+                lblAverage.Text = "";
+                lblScoreCount.Text = "";
+                lblscoreTotal.Text = "";
+                txtScoreInput.Clear();
+                lblMinValue.Text = "";
+                lblMaxValue.Text = "";
+                txtScoreInput.Focus();
+                //A dialog result that takes appropriate actions if the user presses Yes, No or cancel
+            }else if(result == DialogResult.Cancel)
+            {   //A dialogue result that exit the form if the user presses the Yes button
+                DialogResult resultYes =MessageBox.Show("Are you sure you want to cancel", "Confirmation", 
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (resultYes == DialogResult.Yes)
+                {
+                    this.Close();
+                }
+
+            }
+                   
+        }
+        // An action event that exits the form when the user clicks the exit
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want quit?", "Confirmation", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+           
+           
+           if(result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+           else if(result == DialogResult.Cancel)
+            {
+                DialogResult result1 =MessageBox.Show("Are you sure you want to cancel?", "Cancel", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if (result1 == DialogResult.Yes)
+                {
+                    this.Close();
+                }
+            }
+            
+        }
+        //An event handler that display the unsorted scores when clicked
+        private void btnUnscoredGrades_Click(object sender, EventArgs e)
+        {
+            {
+                
+                String grades = " ";
+               
+                foreach (int number in gradeList)
+                {
+
+                    grades += number + "\n";
+
+                }
+                MessageBox.Show(grades, "Unsorted Grades");
+
+            }
+            
+        }
+
+        //A method that sort the grades in the list
+        private List<int> sortedList(List<int> sortedGrads)
+        {
+            List<int> newList = new List<int>(sortedGrads);
+          
+            for (int i = 0; i < newList.Count - 1; i++)
+            {
+                for (int j = 0; j < newList.Count - 1 - i; j++)
+                {
+                    if (newList[j] > newList[j + 1])
+                    {
+                        int temp = newList[j];
+                        newList[j] = newList[j + 1];
+                        newList[j + 1] = temp;
+                    }
+                }
+            }
+            return sortedGrads;
+        }
+        //A method that sorts the score added to the list
+        private void sorrtedScores_Click(object sender, EventArgs e)
+        {
+            String grades = " ";
+            List<int> sortedGrades = new List<int>(gradeList);
+            List<int> gradeSorted = sortedList(sortedGrades);
+            gradeSorted.Sort();
+            foreach (int a in gradeSorted)
+            {
+                grades += a + "\n";
+            }
+            MessageBox.Show(grades, "Sorted Grades");
+        }
+    }
+}
